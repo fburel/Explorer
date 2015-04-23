2c69bc46596ceb1227760eaf511af1db31522368
florian BUREL <florian.burel@mac.com>
2015-04-23 09:16:50 +0200

    Initial Commit

---
+++ Explorer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Explorer
+//
+//  Created by florian BUREL on 23/04/2015.
+//  Copyright (c) 2015 florian BUREL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Explorer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Explorer
+//
+//  Created by florian BUREL on 23/04/2015.
+//  Copyright (c) 2015 florian BUREL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
