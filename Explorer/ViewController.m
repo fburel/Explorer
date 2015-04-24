36efe1e47be0a21b355b3fa44f8a0ed4ce721652
florian BUREL <florian.burel@mac.com>
2015-04-24 14:32:26 +0200

    Affichage des villes

--- Explorer/ViewController.m
+++ Explorer/ViewController.m
@@ -11,6 +11,7 @@
 #import "City.h"
 
 @interface ViewController ()
+<UITableViewDataSource>
 
 @property (strong,nonatomic) CitiesRepository * repository;
 
@@ -23,21 +24,47 @@
 @implementation ViewController
 
 - (IBAction)addCity:(id)sender {
+    
+    [self.repository addCurrentCity];
+    
+    self.cities = [self.repository allCities];
+    
+    [self.tableView reloadData];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.repository = [CitiesRepository new];
-    
+        
     self.cities = [self.repository allCities];
     
-    
+    self.tableView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.cities.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    City * city = self.cities[indexPath.row];
+    
+    UITableViewCell * cell;
+    cell = [tableView dequeueReusableCellWithIdentifier:@"CITY_CELL"
+                                           forIndexPath:indexPath];
+    cell.textLabel.text = city.name;
+    
+    return cell;
+    
 }
 
 @end
